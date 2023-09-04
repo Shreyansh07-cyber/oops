f86a82c01f93fcfdf02c56bcbc9363ff9658abd4
Shreyansh07-cyber <85890408+Shreyansh07-cyber@users.noreply.github.com>
2023-09-03 20:40:09 -0700

    Update Server.java

--- Server.java
+++ Server.java
@@ -19,7 +19,7 @@ public class Server {
         return true;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
             ServerSocket serverSocket = new ServerSocket(3000);
 
@@ -28,61 +28,80 @@ public class Server {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
-
-                InputStream inputStream = clientSocket.getInputStream();
-                OutputStream outputStream = clientSocket.getOutputStream();
-
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                PrintWriter writer = new PrintWriter(outputStream, true);
-
-                String choice = reader.readLine();
-                writer.println("Enter 12 months' salaries separated by commas:");
-                String data = reader.readLine();
-                while (!check(data)) {
-                    writer.println("Error in Input! Enter 12 months' salaries separated by commas:");
-                    data = reader.readLine();
-                }
-
-                if (choice.equalsIgnoreCase("max")) {
-
-                    // String data = reader.readLine();
-                    String[] salariesStr = data.split(",");
-                    double maxSalary = Double.MIN_VALUE;
-                    for (String salaryStr : salariesStr) {
-                        double salary = Double.parseDouble(salaryStr);
-                        if (salary > maxSalary) {
-                            maxSalary = salary;
-                        }
-                    }
-                    writer.println("Maximum salary: " + maxSalary);
-                }
-
-                else if (choice.equalsIgnoreCase("min")) {
-                    // String data = reader.readLine();
-                    String[] salariesStr = data.split(",");
-                    double minSalary = Double.MAX_VALUE;
-                    for (String salaryStr : salariesStr) {
-                        double salary = Double.parseDouble(salaryStr);
-                        if (salary < minSalary) {
-                            minSalary = salary;
-                        }
-                    }
-                    writer.println("Minimum salary: " + minSalary);
-
-                } else if (choice.equalsIgnoreCase("avg")) {
-                    double total, sum = 0;
-                    // String data = reader.readLine();
-                    String[] salariesStr = data.split(",");
-                    for (String salaryStr : salariesStr) {
-                        sum += Double.parseDouble(salaryStr);
-                        // total = total / salariesStr.length;
-                    }
-                    total = (sum / salariesStr.length);
-                    writer.println("average salary: " + total);
-                }
-                clientSocket.close();
-                serverSocket.close();
+                ServerThread st = new ServerThread(clientSocket, serverSocket);
+                st.start();
             }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
+
+class ServerThread extends Thread {
+    Socket clientSocket = null;
+    ServerSocket serverSocket = null;
+
+    public ServerThread(Socket s, ServerSocket ss) {
+        clientSocket = s;
+        serverSocket = ss;
+    }
+
+    public void run() {
+        try {
+            Server se = new Server();
+            InputStream inputStream = clientSocket.getInputStream();
+            OutputStream outputStream = clientSocket.getOutputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            PrintWriter writer = new PrintWriter(outputStream, true);
+
+            String choice = reader.readLine();
+            writer.println("Enter 12 months salaries separated by commas:");
+            String data = reader.readLine();
+            while (!se.check(data)) {
+                writer.println("Error in Input! enter salaries again(seperated by commas):");
+                data = reader.readLine();
+            }
+
+            if (choice.equalsIgnoreCase("max")) {
+
+                // String data = reader.readLine();
+                String[] salariesStr = data.split(",");
+                double maxSalary = Double.MIN_VALUE;
+                for (String salaryStr : salariesStr) {
+                    double salary = Double.parseDouble(salaryStr);
+                    if (salary > maxSalary) {
+                        maxSalary = salary;
+                    }
+                }
+                writer.println("Maximum salary: " + maxSalary);
+            }
+
+            else if (choice.equalsIgnoreCase("min")) {
+                // String data = reader.readLine();
+                String[] salariesStr = data.split(",");
+                double minSalary = Double.MAX_VALUE;
+                for (String salaryStr : salariesStr) {
+                    double salary = Double.parseDouble(salaryStr);
+                    if (salary < minSalary) {
+                        minSalary = salary;
+                    }
+                }
+                writer.println("Minimum salary: " + minSalary);
+
+            } else if (choice.equalsIgnoreCase("avg")) {
+                double total, sum = 0;
+                // String data = reader.readLine();
+                String[] salariesStr = data.split(",");
+                for (String salaryStr : salariesStr) {
+                    sum += Double.parseDouble(salaryStr);
+                    // total = total / salariesStr.length;
+                }
+                total = (sum / salariesStr.length);
+                writer.println("average salary: " + total);
+            }
+            clientSocket.close();
+            serverSocket.close();
         } catch (IOException e) {
             System.out.println(e);
         }
